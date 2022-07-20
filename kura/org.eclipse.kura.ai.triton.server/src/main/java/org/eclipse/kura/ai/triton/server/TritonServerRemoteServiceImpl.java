a0296a6ce7aba5185eb2c2a7db6a3f9e6ef2010c
Mattia Dal Ben <matthewdibi@gmail.com>
2022-07-20 15:15:33 +0200

    feat: add TritonServerRemoteService skeleton draft

---
+++ kura/org.eclipse.kura.ai.triton.server/src/main/java/org/eclipse/kura/ai/triton/server/TritonServerRemoteServiceImpl.java
@@ -0,0 +1,10 @@
+package org.eclipse.kura.ai.triton.server;
+
+public class TritonServerRemoteServiceImpl extends TritonServerServiceAbs {
+
+    @Override
+    String buildSharedString() {
+        return "Remote";
+    }
+
+}
