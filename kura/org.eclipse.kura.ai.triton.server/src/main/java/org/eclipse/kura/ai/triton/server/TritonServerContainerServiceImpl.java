ad8259eb94ea428e0fe599192cc97b48df0e04ab
Mattia Dal Ben <matthewdibi@gmail.com>
2022-07-21 14:30:43 +0200

    feat: add TritonServerContainerService skeleton draft

---
+++ kura/org.eclipse.kura.ai.triton.server/src/main/java/org/eclipse/kura/ai/triton/server/TritonServerContainerServiceImpl.java
@@ -0,0 +1,10 @@
+package org.eclipse.kura.ai.triton.server;
+
+public class TritonServerContainerServiceImpl extends TritonServerServiceAbs {
+
+    @Override
+    String buildSharedString() {
+        return "Container";
+    }
+
+}
