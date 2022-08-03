797b8b795deba1722e236d8bd0f5eb0dc862809a
Mattia Dal Ben <matthewdibi@gmail.com>
2022-08-03 09:13:08 +0200

    feat(ai.triton.server): add quick&dirty TritonServerContainerService

---
+++ kura/org.eclipse.kura.ai.triton.server/src/main/java/org/eclipse/kura/ai/triton/server/TritonServerServiceContainerImpl.java
@@ -0,0 +1,29 @@
+package org.eclipse.kura.ai.triton.server;
+
+import org.eclipse.kura.container.orchestration.ContainerOrchestrationService;
+import org.eclipse.kura.executor.CommandExecutorService;
+
+public class TritonServerServiceContainerImpl extends TritonServerServiceAbs {
+
+    @Override
+    TritonServerInstanceManager createInstanceManager(TritonServerServiceOptions options,
+            CommandExecutorService executorService, ContainerOrchestrationService containerOrchestrationService,
+            String decryptionFolderPath) {
+        return new TritonServerContainerManager(options, containerOrchestrationService, decryptionFolderPath);
+    }
+
+    @Override
+    boolean isConfigurationValid() {
+        return !isNullOrEmpty(this.options.getBackendsPath()) && !isNullOrEmpty(this.options.getModelRepositoryPath());
+    }
+
+    @Override
+    boolean isModelEncryptionEnabled() {
+        return false; // Temporarily disabled
+    }
+
+    @Override
+    String getServerAddress() {
+        return "localhost";
+    }
+}
