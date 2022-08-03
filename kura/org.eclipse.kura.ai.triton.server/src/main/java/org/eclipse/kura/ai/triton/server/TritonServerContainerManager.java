a5c179655d02549c501d508f57e5be4137b3bb4a
Mattia Dal Ben <matthewdibi@gmail.com>
2022-08-03 09:07:41 +0200

    feat(ai.triton.server): add quick&dirty TritonServerContainerManager

---
+++ kura/org.eclipse.kura.ai.triton.server/src/main/java/org/eclipse/kura/ai/triton/server/TritonServerContainerManager.java
@@ -0,0 +1,155 @@
+package org.eclipse.kura.ai.triton.server;
+
+import static java.util.Objects.nonNull;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+
+import org.eclipse.kura.KuraException;
+import org.eclipse.kura.container.orchestration.ContainerConfiguration;
+import org.eclipse.kura.container.orchestration.ContainerConfiguration.ContainerConfigurationBuilder;
+import org.eclipse.kura.container.orchestration.ContainerOrchestrationService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TritonServerContainerManager implements TritonServerInstanceManager {
+
+    private static final Logger logger = LoggerFactory.getLogger(TritonServerContainerManager.class);
+
+    private static final int MONITOR_PERIOD = 30;
+
+    private final String decryptionFolderPath;
+    private final ContainerOrchestrationService containerOrchestrationService;
+    private final TritonServerServiceOptions options;
+    private String containerID = "";
+
+    private ScheduledExecutorService scheduledExecutorService;
+    private ScheduledFuture<?> scheduledFuture;
+
+    protected TritonServerContainerManager(TritonServerServiceOptions options,
+            ContainerOrchestrationService containerOrchestrationService, String decryptionFolderPath) {
+        this.options = options;
+        this.containerOrchestrationService = containerOrchestrationService;
+        this.decryptionFolderPath = decryptionFolderPath;
+        this.scheduledExecutorService = Executors.newSingleThreadScheduledExecutor();
+    }
+
+    @Override
+    public void start() {
+        startContainerServerMonitor();
+    }
+
+    @Override
+    public void stop() {
+        stopLocalServerMonitor();
+        stopScheduledExecutor();
+    }
+
+    @Override
+    public void kill() {
+        killLocalServerMonitor();
+        stopScheduledExecutor();
+    }
+
+    private void startContainerServerMonitor() {
+        this.scheduledFuture = this.scheduledExecutorService.scheduleAtFixedRate(() -> {
+            Thread.currentThread().setName(getClass().getSimpleName());
+            if (!isServerRunning()) {
+                startLocalServer();
+            }
+        }, 0, MONITOR_PERIOD, TimeUnit.SECONDS);
+    }
+
+    private void startLocalServer() {
+        ContainerConfiguration containerConfiguration = createContainerConfiguration();
+        try {
+            this.containerID = this.containerOrchestrationService.startContainer(containerConfiguration);
+        } catch (KuraException | InterruptedException e) {
+            logger.info("Nvidia Triton Container not started. {}", e);
+        }
+
+        logger.info("Nvidia Triton Container started. Container ID: {}", containerID);
+    }
+
+    private void stopLocalServerMonitor() {
+        stopMonitor();
+        stopLocalServer();
+    }
+
+    private void killLocalServerMonitor() {
+        stopMonitor();
+        killLocalServer();
+    }
+
+    private void stopMonitor() {
+        if (nonNull(this.scheduledFuture)) {
+            this.scheduledFuture.cancel(true);
+            while (!this.scheduledFuture.isDone()) {
+                sleepFor(500);
+            }
+        }
+    }
+
+    private synchronized void stopLocalServer() {
+        try {
+            this.containerOrchestrationService.stopContainer(containerID);
+            this.containerID = "";
+        } catch (KuraException e) {
+            logger.error("Can't stop {} won't stop LOL", containerID);
+        }
+    }
+
+    private synchronized void killLocalServer() {
+        stopLocalServer();
+    }
+
+    private void stopScheduledExecutor() {
+        if (this.scheduledExecutorService != null) {
+            this.scheduledExecutorService.shutdown();
+            while (!this.scheduledExecutorService.isTerminated()) {
+                sleepFor(500);
+            }
+            this.scheduledExecutorService = null;
+        }
+    }
+
+    @Override
+    public boolean isServerRunning() {
+        return !this.containerID.isEmpty();
+    }
+
+    private static void sleepFor(long timeout) {
+        try {
+            Thread.sleep(timeout);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            logger.debug(e.getMessage(), e);
+        }
+    }
+
+    private ContainerConfiguration createContainerConfiguration() {
+        ContainerConfigurationBuilder builder = new ContainerConfigurationBuilder();
+
+        builder.setContainerImage("nvcr.io/nvidia/tritonserver");
+        builder.setContainerImageTag("22.02-py3");
+        builder.setFrameworkManaged(true);
+        builder.setInternalPorts(Arrays.asList(8000, 8001, 8002));
+        builder.setExternalPorts(
+                Arrays.asList(this.options.getHttpPort(), this.options.getGrpcPort(), this.options.getMetricsPort()));
+
+        Map<String, String> map = new HashMap<>();
+        map.put(this.options.getModelRepositoryPath(), "/models");
+        builder.setVolumes(map);
+
+        builder.setEntryPoint(
+                Arrays.asList("tritonserver", "--model-repository=/models", "--model-control-mode=explicit"));
+
+        return builder.build();
+    }
+
+}
