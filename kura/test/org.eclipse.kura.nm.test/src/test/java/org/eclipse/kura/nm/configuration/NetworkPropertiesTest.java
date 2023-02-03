484de7fa001379b77d881975d03d8c98ed627a1a
Gregory Ivo <gregorybivo@gmail.com>
2023-02-03 09:41:50 -0500

    tests: added import for Arrays

--- kura/test/org.eclipse.kura.nm.test/src/test/java/org/eclipse/kura/nm/configuration/NetworkPropertiesTest.java
+++ kura/test/org.eclipse.kura.nm.test/src/test/java/org/eclipse/kura/nm/configuration/NetworkPropertiesTest.java
@@ -15,6 +15,7 @@ package org.eclipse.kura.nm.configuration;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
