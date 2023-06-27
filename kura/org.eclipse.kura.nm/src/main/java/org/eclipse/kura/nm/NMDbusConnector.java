b1df0982c8142eaa4dc7f11ad98ab29fc1fdcd47
Mattia Dal Ben <matthewdibi@gmail.com>
2023-06-27 09:41:03 +0200

    fix: remove unused const

--- kura/org.eclipse.kura.nm/src/main/java/org/eclipse/kura/nm/NMDbusConnector.java
+++ kura/org.eclipse.kura.nm/src/main/java/org/eclipse/kura/nm/NMDbusConnector.java
@@ -71,7 +71,6 @@ public class NMDbusConnector {
     private static final String NM_BUS_PATH = "/org/freedesktop/NetworkManager";
     private static final String NM_DEVICE_BUS_NAME = "org.freedesktop.NetworkManager.Device";
     private static final String NM_DEVICE_WIRELESS_BUS_NAME = "org.freedesktop.NetworkManager.Device.Wireless";
-    private static final String NM_GENERIC_DEVICE_BUS_NAME = "org.freedesktop.NetworkManager.Device.Generic";
     private static final String NM_SETTINGS_BUS_PATH = "/org/freedesktop/NetworkManager/Settings";
     private static final String MM_BUS_NAME = "org.freedesktop.ModemManager1";
     private static final String MM_BUS_PATH = "/org/freedesktop/ModemManager1";
@@ -82,12 +81,8 @@ public class NMDbusConnector {
     private static final String NM_PROPERTY_VERSION = "Version";
 
     private static final String NM_DEVICE_PROPERTY_INTERFACE = "Interface";
-    private static final String NM_DEVICE_PROPERTY_MANAGED = "Managed";
     private static final String NM_DEVICE_PROPERTY_DEVICETYPE = "DeviceType";
-    private static final String NM_DEVICE_PROPERTY_STATE = "State";
     private static final String NM_DEVICE_PROPERTY_IP4CONFIG = "Ip4Config";
-    private static final String NM_SETTING_CONNECTION_KEY = "connection";
-    private static final String NM_DEVICE_GENERIC_PROPERTY_TYPEDESCRIPTION = "TypeDescription";
 
     private static final String MM_MODEM_PROPERTY_STATE = "State";
 
