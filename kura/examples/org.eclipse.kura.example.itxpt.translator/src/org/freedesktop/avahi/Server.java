59e197239cbfb65354a83910ac6dbc5009813905
Mattia Dal Ben <matthewdibi@gmail.com>
2024-08-12 10:53:58 +0200

    refactor: remove unused import + styling

--- kura/examples/org.eclipse.kura.example.itxpt.translator/src/org/freedesktop/avahi/Server.java
+++ kura/examples/org.eclipse.kura.example.itxpt.translator/src/org/freedesktop/avahi/Server.java
@@ -1,6 +1,5 @@
 package org.freedesktop.avahi;
 
-import java.util.List;
 import org.freedesktop.dbus.DBusPath;
 import org.freedesktop.dbus.annotations.DBusInterfaceName;
 import org.freedesktop.dbus.exceptions.DBusException;
@@ -15,32 +14,57 @@ import org.freedesktop.dbus.types.UInt32;
 @DBusInterfaceName("org.freedesktop.Avahi.Server")
 public interface Server extends DBusInterface {
 
-
     public String GetVersionString();
-    public UInt32 GetAPIVersion();
-    public String GetHostName();
-    public void SetHostName(String name);
-    public String GetHostNameFqdn();
-    public String GetDomainName();
-    public boolean IsNSSSupportAvailable();
-    public int GetState();
-    public UInt32 GetLocalServiceCookie();
-    public String GetAlternativeHostName(String name);
-    public String GetAlternativeServiceName(String name);
-    public String GetNetworkInterfaceNameByIndex(int index);
-    public int GetNetworkInterfaceIndexByName(String name);
-    public ResolveHostNameTuple ResolveHostName(int interfaceparam, int protocol, String name, int aprotocol, UInt32 flags);
-    public ResolveAddressTuple ResolveAddress(int interfaceparam, int protocol, String address, UInt32 flags);
-    public ResolveServiceTuple ResolveService(int interfaceparam, int protocol, String name, String type, String domain, int aprotocol, UInt32 flags);
-    public DBusPath EntryGroupNew();
-    public DBusPath DomainBrowserNew(int interfaceparam, int protocol, String domain, int btype, UInt32 flags);
-    public DBusPath ServiceTypeBrowserNew(int interfaceparam, int protocol, String domain, UInt32 flags);
-    public DBusPath ServiceBrowserNew(int interfaceparam, int protocol, String type, String domain, UInt32 flags);
-    public DBusPath ServiceResolverNew(int interfaceparam, int protocol, String name, String type, String domain, int aprotocol, UInt32 flags);
-    public DBusPath HostNameResolverNew(int interfaceparam, int protocol, String name, int aprotocol, UInt32 flags);
-    public DBusPath AddressResolverNew(int interfaceparam, int protocol, String address, UInt32 flags);
-    public DBusPath RecordBrowserNew(int interfaceparam, int protocol, String name, UInt16 clazz, UInt16 type, UInt32 flags);
 
+    public UInt32 GetAPIVersion();
+
+    public String GetHostName();
+
+    public void SetHostName(String name);
+
+    public String GetHostNameFqdn();
+
+    public String GetDomainName();
+
+    public boolean IsNSSSupportAvailable();
+
+    public int GetState();
+
+    public UInt32 GetLocalServiceCookie();
+
+    public String GetAlternativeHostName(String name);
+
+    public String GetAlternativeServiceName(String name);
+
+    public String GetNetworkInterfaceNameByIndex(int index);
+
+    public int GetNetworkInterfaceIndexByName(String name);
+
+    public ResolveHostNameTuple ResolveHostName(int interfaceparam, int protocol, String name, int aprotocol,
+            UInt32 flags);
+
+    public ResolveAddressTuple ResolveAddress(int interfaceparam, int protocol, String address, UInt32 flags);
+
+    public ResolveServiceTuple ResolveService(int interfaceparam, int protocol, String name, String type, String domain,
+            int aprotocol, UInt32 flags);
+
+    public DBusPath EntryGroupNew();
+
+    public DBusPath DomainBrowserNew(int interfaceparam, int protocol, String domain, int btype, UInt32 flags);
+
+    public DBusPath ServiceTypeBrowserNew(int interfaceparam, int protocol, String domain, UInt32 flags);
+
+    public DBusPath ServiceBrowserNew(int interfaceparam, int protocol, String type, String domain, UInt32 flags);
+
+    public DBusPath ServiceResolverNew(int interfaceparam, int protocol, String name, String type, String domain,
+            int aprotocol, UInt32 flags);
+
+    public DBusPath HostNameResolverNew(int interfaceparam, int protocol, String name, int aprotocol, UInt32 flags);
+
+    public DBusPath AddressResolverNew(int interfaceparam, int protocol, String address, UInt32 flags);
+
+    public DBusPath RecordBrowserNew(int interfaceparam, int protocol, String name, UInt16 clazz, UInt16 type,
+            UInt32 flags);
 
     public static class StateChanged extends DBusSignal {
 
@@ -53,7 +77,6 @@ public interface Server extends DBusInterface {
             this.error = _error;
         }
 
-
         public int getState() {
             return state;
         }
@@ -61,7 +84,6 @@ public interface Server extends DBusInterface {
         public String getError() {
             return error;
         }
-
 
     }
 }
