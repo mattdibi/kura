088a24c5cc6d8d99f353df5c200e8b605146b9da
Mattia Dal Ben <matthewdibi@gmail.com>
2024-08-12 10:46:44 +0200

    fix: DomainBrowser.java autogen code

--- kura/examples/org.eclipse.kura.example.itxpt.translator/src/org/freedesktop/avahi/DomainBrowser.java
+++ kura/examples/org.eclipse.kura.example.itxpt.translator/src/org/freedesktop/avahi/DomainBrowser.java
@@ -12,78 +12,75 @@ import org.freedesktop.dbus.types.UInt32;
 @DBusInterfaceName("org.freedesktop.Avahi.DomainBrowser")
 public interface DomainBrowser extends DBusInterface {
 
-
     public void Free();
-    public void Start();
 
+    public void Start();
 
     public static class ItemNew extends DBusSignal {
 
-        private final int interfaceparam;
+        private final int iface;
         private final int protocol;
         private final String domain;
         private final UInt32 flags;
 
-        public ItemNew(String _path, int _interface, int _protocol, String _domain, UInt32 _flags) throws DBusException {
+        public ItemNew(String _path, int _interface, int _protocol, String _domain, UInt32 _flags)
+                throws DBusException {
             super(_path, _interface, _protocol, _domain, _flags);
-            this.interface = _interface;
+            this.iface = _interface;
             this.protocol = _protocol;
             this.domain = _domain;
             this.flags = _flags;
         }
 
-
-        public int getInterfaceparam() {
-            return interfaceparam;
+        public int getIface() {
+            return this.iface;
         }
 
         public int getProtocol() {
-            return protocol;
+            return this.protocol;
         }
 
         public String getDomain() {
-            return domain;
+            return this.domain;
         }
 
-        public UInt32 getFlags() {
-            return flags;
+        public UInt32 getItemFlags() {
+            return this.flags;
         }
-
 
     }
 
     public static class ItemRemove extends DBusSignal {
 
-        private final int interfaceparam;
+        private final int iface;
         private final int protocol;
         private final String domain;
         private final UInt32 flags;
 
-        public ItemRemove(String _path, int _interface, int _protocol, String _domain, UInt32 _flags) throws DBusException {
+        public ItemRemove(String _path, int _interface, int _protocol, String _domain, UInt32 _flags)
+                throws DBusException {
             super(_path, _interface, _protocol, _domain, _flags);
-            this.interface = _interface;
+            this.iface = _interface;
             this.protocol = _protocol;
             this.domain = _domain;
             this.flags = _flags;
         }
 
-
-        public int getInterfaceparam() {
-            return interfaceparam;
+        public int getIface() {
+            return this.iface;
         }
 
         public int getProtocol() {
-            return protocol;
+            return this.protocol;
         }
 
         public String getDomain() {
-            return domain;
+            return this.domain;
         }
 
-        public UInt32 getFlags() {
-            return flags;
+        public UInt32 getItemFlags() {
+            return this.flags;
         }
-
 
     }
 
@@ -96,11 +93,9 @@ public interface DomainBrowser extends DBusInterface {
             this.error = _error;
         }
 
-
         public String getError() {
             return error;
         }
-
 
     }
 }
