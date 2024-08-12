040f0417eb5d0ac8ff181253f2b68f6e1d938c3a
Mattia Dal Ben <matthewdibi@gmail.com>
2024-08-12 12:05:12 +0200

    feat: first working example of Avahi Daemon dbus api call

--- kura/examples/org.eclipse.kura.example.itxpt.translator/src/org/eclipse/kura/example/itxpt/translator/ITxPTTranslator.java
+++ kura/examples/org.eclipse.kura.example.itxpt.translator/src/org/eclipse/kura/example/itxpt/translator/ITxPTTranslator.java
@@ -1,5 +1,10 @@
 package org.eclipse.kura.example.itxpt.translator;
 
+import java.io.IOException;
+
+import org.freedesktop.avahi.Server2;
+import org.freedesktop.dbus.connections.impl.DBusConnection;
+import org.freedesktop.dbus.exceptions.DBusException;
 import org.osgi.service.component.ComponentContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,6 +17,16 @@ public class ITxPTTranslator {
 
     protected void activate(ComponentContext componentContext) {
         s_logger.info("Bundle " + APP_ID + " has started!");
+
+        try (DBusConnection dbusConn = DBusConnection.getConnection(DBusConnection.DEFAULT_SYSTEM_BUS_ADDRESS)) {
+            Server2 server = dbusConn.getRemoteObject("org.freedesktop.Avahi", "/", Server2.class);
+
+            String version = server.GetVersionString();
+
+            s_logger.info("Detected Avahi Daemon version: {}", version);
+        } catch (IOException | DBusException _ex) {
+            _ex.printStackTrace();
+        }
 
         s_logger.info("Registered DNS service");
     }
