81960e3824ef904a27560abd83468d659a146267
Mattia Dal Ben <matthewdibi@gmail.com>
2024-08-12 11:00:10 +0200

    fix: ServiceResolver.java autogen code

--- kura/examples/org.eclipse.kura.example.itxpt.translator/src/org/freedesktop/avahi/ServiceResolver.java
+++ kura/examples/org.eclipse.kura.example.itxpt.translator/src/org/freedesktop/avahi/ServiceResolver.java
@@ -1,6 +1,7 @@
 package org.freedesktop.avahi;
 
 import java.util.List;
+
 import org.freedesktop.dbus.annotations.DBusInterfaceName;
 import org.freedesktop.dbus.exceptions.DBusException;
 import org.freedesktop.dbus.interfaces.DBusInterface;
@@ -14,14 +15,13 @@ import org.freedesktop.dbus.types.UInt32;
 @DBusInterfaceName("org.freedesktop.Avahi.ServiceResolver")
 public interface ServiceResolver extends DBusInterface {
 
-
     public void Free();
-    public void Start();
 
+    public void Start();
 
     public static class Found extends DBusSignal {
 
-        private final int interfaceparam;
+        private final int iface;
         private final int protocol;
         private final String name;
         private final String type;
@@ -33,9 +33,12 @@ public interface ServiceResolver extends DBusInterface {
         private final List<List<Byte>> txt;
         private final UInt32 flags;
 
-        public Found(String _path, int _interface, int _protocol, String _name, String _type, String _domain, String _host, int _aprotocol, String _address, UInt16 _port, List<List<Byte>> _txt, UInt32 _flags) throws DBusException {
-            super(_path, _interface, _protocol, _name, _type, _domain, _host, _aprotocol, _address, _port, _txt, _flags);
-            this.interface = _interface;
+        public Found(String _path, int _interface, int _protocol, String _name, String _type, String _domain,
+                String _host, int _aprotocol, String _address, UInt16 _port, List<List<Byte>> _txt, UInt32 _flags)
+                throws DBusException {
+            super(_path, _interface, _protocol, _name, _type, _domain, _host, _aprotocol, _address, _port, _txt,
+                    _flags);
+            this.iface = _interface;
             this.protocol = _protocol;
             this.name = _name;
             this.type = _type;
@@ -48,51 +51,49 @@ public interface ServiceResolver extends DBusInterface {
             this.flags = _flags;
         }
 
-
-        public int getInterfaceparam() {
-            return interfaceparam;
+        public int getIface() {
+            return this.iface;
         }
 
         public int getProtocol() {
-            return protocol;
+            return this.protocol;
         }
 
-        public String getName() {
-            return name;
+        public String getAddressName() {
+            return this.name;
         }
 
-        public String getType() {
-            return type;
+        public String getAddressType() {
+            return this.type;
         }
 
         public String getDomain() {
-            return domain;
+            return this.domain;
         }
 
         public String getHost() {
-            return host;
+            return this.host;
         }
 
         public int getAprotocol() {
-            return aprotocol;
+            return this.aprotocol;
         }
 
         public String getAddress() {
-            return address;
+            return this.address;
         }
 
         public UInt16 getPort() {
-            return port;
+            return this.port;
         }
 
         public List<List<Byte>> getTxt() {
-            return txt;
+            return this.txt;
         }
 
-        public UInt32 getFlags() {
-            return flags;
+        public UInt32 getSignalFlags() {
+            return this.flags;
         }
-
 
     }
 
@@ -105,11 +106,9 @@ public interface ServiceResolver extends DBusInterface {
             this.error = _error;
         }
 
-
         public String getError() {
             return error;
         }
-
 
     }
 }
